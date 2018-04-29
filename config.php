8f3bf4d42392c3e47c84062b870a422e1ee687b9
s0ft4u <ndkhoa150193@gmail.com>
2018-04-30 01:01:21 +0700

    Add config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+/*!
+ * @author		Sebastian Lutz
+ * @copyright	Baebeca Solutions - Lutz
+ * @email		lutz@baebeca.de
+ * @pgp			0x5AD0240C
+ * @link		https://www.baebeca.de
+ * @link-github	https://github.com/s0ft4u/antpool-php-api
+ * @customer	-
+ * @project		antpool-php-api
+ * @license		GNU GENERAL PUBLIC LICENSE Version 2
+ **/
+
+class ant_config {
+
+	// antpool user settings
+	public $username 	= 's0ft4u';
+	public $api_key 	= '49dff2cb115144b68faa59db285acd59';
+	public $api_secret 	= '960cf6903e6b4d0f8292bb2b9ee184e5';
+
+	// alerts
+	public  $email 		= 'ndkhoa150193@gmail.com';
+	public  $mobile 	= '';
+
+}
