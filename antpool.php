629e7baf8301bca33c307706891a7f9c753b70a0
s0ft4u <ndkhoa150193@gmail.com>
2019-08-18 00:12:25 +0900

    Update antpool.php

--- antpool.php
+++ antpool.php
@@ -1,36 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
-/*!
- * @author		Sebastian Lutz
- * @copyright	Baebeca Solutions - Lutz
- * @email		lutz@baebeca.de
- * @pgp			0x5AD0240C
- * @link		https://www.baebeca.de
- * @link-github	https://github.com/Elompenta/antpool-php-api
- * @customer	-
- * @project		antpool-php-api
- * @license		GNU GENERAL PUBLIC LICENSE Version 2
- **/
+echo "Hello World!";
+?> 
 
-// load classes
-error_reporting(E_ALL);
-require_once(__DIR__.'/classes/antpool.php');
-
-// check if custom config file exist
-if (file_exists(__DIR__.'/config.php')) {
-	require_once(__DIR__.'/config.php');
-} else {
-	exit('please create your own config.php based on config.sample.php');
-}
-
-// init antpool class
-$ant_config = new ant_config();
-$ant 	= new antpool($ant_config->username, $ant_config->api_key, $ant_config->api_secret);
-
-// check if custom.php exist and execute
-if (file_exists(__DIR__.'/custom.php')) {
-	require_once(__DIR__.'/custom.php');
-} else {
-	require_once(__DIR__.'/custom.sample.php');
-}
-
-unset($ant);
+</body>
+</html>
